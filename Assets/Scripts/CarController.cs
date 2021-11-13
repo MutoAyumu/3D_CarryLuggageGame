ff52d2a92fb108c448a61a1e6ce0f7fa30dbbedc
MutoAyumu <mutoayum0610@gmail.com>
2021-11-14 04:09:39 +0900

    最初のコミット

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    bool isOn;
+
+    private void FixedUpdate()
+    {
+        if (isOn)
+            Debug.Log("タッチされた");
+    }
+    public void Down()
+    {
+        isOn = true;
+    }
+    public void Up()
+    {
+        isOn = false;
+    }
+}
