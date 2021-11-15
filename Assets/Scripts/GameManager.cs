510466db004f8c0b21cf3974ffd42834bc0621a7
MutoAyumu <mutoayum0610@gmail.com>
2021-11-16 04:17:11 +0900

    GameManagerの作成

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    float _currntScore;
+
+    [SerializeField] Text _scoreText = default;
+
+    private void Start()
+    {
+        _scoreText.text = _currntScore.ToString();
+    }
+
+    public void AddScore(float score)
+    {
+        _currntScore += score;
+        _scoreText.text = _currntScore.ToString();
+    }
+}
---
+++ Assets/Scripts/ScoreItem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreItem : MonoBehaviour
+{
+    [SerializeField] float _score = 100f;
+    GameManager _gameManager = default;
+
+    private void Start()
+    {
+        _gameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(_gameManager == null)
+            {
+                Debug.LogError("ScoreSystemが取得出来ていません");
+            }
+            else
+            {
+                _gameManager.AddScore(_score);
+            }
+
+            Destroy(this.gameObject);
+        }
+    }
+}
